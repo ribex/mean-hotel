3d4fa7db2d516f7f06ddbba8b0105d4f691df8ec
Rebecca Wagaman <ribexy@gmail.com>
2017-09-12 00:10:38 +0000

    Begin separation of concerns, requiring external js in app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-var name = "Everyone!";
-console.log("Hello " + name);
+// use './' because node looks in node modules folder otherwise
+// filename does not need .js - you can use it but best practice not to
+require('./instantHello');
