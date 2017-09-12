ba0260709c0e381e1d2f71120637599ce05af6f7
Rebecca Wagaman <ribexy@gmail.com>
2017-09-12 01:02:22 +0000

    Add another way to use private variable inside a file

--- talk/index.js
+++ talk/index.js
@@ -1,9 +1,12 @@
+// this variable isn't exposed outside the file - app.js "doesn't know it exists"
+var filename = "index.js";
+
 var hello = function(name) {
     console.log("Hello " + name);
 };
 
 var intro = function() {
-    console.log("I'm a node file called index.js");
+    console.log("I'm a node file called " + filename);
 };
 
 // expose the methods (otherwise they are private to this file)
