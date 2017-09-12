d57ba8081ba837c1f8790bb877a040a219661b6c
Rebecca Wagaman <ribexy@gmail.com>
2017-09-12 00:15:32 +0000

    Add another way to expose a method and call it

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // use './' because node looks in node modules folder otherwise
 // filename does not need .js - you can use it but best practice not to
 require('./instantHello');
+// another way to expose a method 
+var goodbye = require('./talk/goodbye');
+// now to call the method
+goodbye();
