c8571019ef4cbe9980e8b5d3b9cfc4faa1c2a20e
Rebecca Wagaman <ribexy@gmail.com>
2017-09-16 16:55:52 +0000

    Adjust port settings to work with cloud9

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var express = require('express');
 // initialize express
 var app = express();
 // eliminate hard-coded port number
-app.set('port', 3000);
+app.set('port', process.env.PORT);
+
+app.get('/', function(req, res) {
+    console.log("GET the homepage");
+    res.send("Express yourself");
+});
+
 // give express a port to listen for requests
 // app.listen returns an object, so assign it to a variable to be able to access properties of the server object
 var server = app.listen(app.get('port'), function() {
