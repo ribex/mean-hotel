efcf64f370c0f2689c15cd828897cbc9c2739f32
Rebecca Wagaman <ribexy@gmail.com>
2017-09-16 17:27:13 +0000

    Add index.html and routing to serve up this file from the default path

--- app.js
+++ app.js
@@ -2,21 +2,38 @@
 var express = require('express');
 // initialize express
 var app = express();
+
+var path = require('path');
 // eliminate hard-coded port number
 app.set('port', process.env.PORT);
 
-app.get('/', function(req, res) {
-    console.log("GET the homepage");
-    res
-        .status(404)
-        .send("Express yourself");
-});
+// app.get('/', function(req, res) {
+//     console.log("GET the homepage");
+//     res
+//         .status(404)
+//         .send("Express yourself");
+// });
 
 app.get('/json', function(req, res) {
     console.log("GET the json");
     res
         .status(200)
         .json({"jsonData" : true});
+});
+
+app.get('/file', function(req, res) {
+    console.log("GET the file");
+    res
+        .status(200)
+        // create file path
+        .sendFile(path.join(__dirname, 'app.js'));
+});
+
+app.get('/', function(req, res) {
+   console.log("GET the homepage");
+   res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // give express a port to listen for requests
