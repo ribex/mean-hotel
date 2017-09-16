92a3155f6a5f7c91a3b727976458a9cea5a12910
Rebecca Wagaman <ribexy@gmail.com>
2017-09-16 14:43:30 +0000

    Add named function to demonstrate asynchronicity

--- readFileAsync.js
+++ readFileAsync.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 
-console.log("Going to get a file");
-fs.readFile('readFileSync.js', function(err, file) {
+var onFileLoad = function(err, file) {
     console.log("Got the file");
-});
 
+};
+
+console.log("Going to get a file");
+fs.readFile('readFileSync.js', onFileLoad);
 console.log("App continues...");
