e66e92f1787d86705c33bdfdedf311ab5798a481
Rebecca Wagaman <ribexy@gmail.com>
2017-09-16 23:53:01 +0000

    Display the hotel data at /api/hotels

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,10 @@
 // create controller that will run when /api/controllers is called
+var hotelData = require('../data/hotel-data.json');
+
 module.exports.hotelsGetAll = function(req, res) {
   // define function to run; can chain multiple methods (such as POST) to a single route
-  console.log("GET the json");
+  console.log("GET the hotels");
   res
     .status(200)
-    .json({"jsonData" : true});
+    .json( hotelData );
 };
