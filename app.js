2e4684341fdb20f753fead3586df519475b23ecb
Rebecca Wagaman <ribexy@gmail.com>
2017-09-16 15:41:02 +0000

    Assign functions to variables to access object properties and methods

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var app = express();
 // eliminate hard-coded port number
 app.set('port', 3000);
 // give express a port to listen for requests
-app.listen(app.get('port'));
-// confirm with console log - view on http://localhost:3000
-console.log('Magic happens on port ' + app.get('port'));
+// app.listen returns an object, so assign it to a variable to be able to access properties of the server object
+var server = app.listen(app.get('port'), function() {
+    // extract port number from server object
+    var port = server.address().port;
+    // confirm with console log - view on http://localhost:3000
+    // async in anonymous callback function
+    console.log('Magic happens on port ' + app.get('port'));
+});
+// appears before the app.listen anon function
+console.log('Me first!');
